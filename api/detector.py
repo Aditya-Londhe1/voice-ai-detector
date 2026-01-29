7b8e9de24c990558cfb6db4c5fe9bf7b04303893
Aditya Londhe <aditya.londhe24@sanjivani.edu.in>
2026-01-30 02:15:10 +0530

    Update detector.py

--- api/detector.py
+++ api/detector.py
@@ -1,8 +1,9 @@
 import torch
-import librosa
 import base64
 import io
 import numpy as np
+import soundfile as sf
+import librosa
 
 import sys
 import os
@@ -11,7 +12,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from rawnet.model import RawNetLite
 from rawnet.features import extract_mfcc
-
 
 
 class VoiceDetector:
@@ -34,35 +34,65 @@ class VoiceDetector:
 
     def decode_audio(self, base64_audio):
 
+        # Decode base64
         audio_bytes = base64.b64decode(base64_audio)
 
+        # Convert to in-memory file
         buffer = io.BytesIO(audio_bytes)
 
-        audio, _ = librosa.load(buffer, sr=16000)
+        try:
+            # Use soundfile (more stable than librosa)
+            audio, sr = sf.read(buffer)
+
+        except Exception:
+            raise ValueError("Cannot decode WAV file")
+
+
+        # Convert stereo to mono
+        if len(audio.shape) > 1:
+            audio = np.mean(audio, axis=1)
+
+
+        # Resample if needed
+        if sr != 16000:
+            audio = librosa.resample(
+                audio,
+                orig_sr=sr,
+                target_sr=16000
+            )
+
 
         return audio
 
 
     def predict(self, base64_audio):
 
+        # Decode
         audio = self.decode_audio(base64_audio)
 
-        # Fix length
+
+        # Fix length (at least 1 second)
         if len(audio) < 16000:
             audio = librosa.util.fix_length(audio, 16000)
+
 
         # Extract MFCC
         mfcc = extract_mfcc(audio)
 
+
+        # To tensor
         x = torch.tensor(mfcc)\
                 .unsqueeze(0)\
                 .float()\
                 .to(self.device)
 
+
+        # Predict
         with torch.no_grad():
 
             out = self.model(x)
 
             prob = torch.sigmoid(out).item()
+
 
         return prob
